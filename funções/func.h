6c5d618c7afc1fe35b061323abcaee47baee64de
Gustavo Oly <gustavooly.dev@gmail.com>
2024-01-25 18:42:56 -0300

    feat: combinação de algoritmos de tomadas de decições possíveis em c

---
+++ funções/func.h
@@ -0,0 +1,6 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+int somar(int a, int b);
+
+#endif
--- funções/script.c
+++ funções/script.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include "func.h"
 
 int adicao(int num1, int num2);
 int sub(int num1, int num2);
@@ -8,11 +9,18 @@ bool verd(bool num1, bool num2);
 
 int main(int argc, char const *argv[])
 {
-    
+    char thola;
+    int resultado = somar(4,3);
+    printf("valor: %d\n", resultado);
     printf("%d \n",adicao(1,3));
     printf("%d \n", sub(4,2));
     printf("%.1f \n", mult(2,2));
     verd(true, true);
+
+    thola = "";
+    printf("%c Escreva uma coisa \n ");
+    scanf("%c", thola);
+    printf("%c você é um: \n", thola);
     return 0;
 }
 
@@ -29,12 +37,5 @@ float mult(float num1, float num2){
 }
 
 bool verd(bool num1, bool num2){
-    if (num1 ^ num2 == true)
-    {
-        printf("Verdadeiro");
-    }
-    else
-    {
-        printf("Falso");
-    }
+    if (num1 ^ num2 == true) {printf("Verdadeiro");} else{printf("Falso");}
 }
