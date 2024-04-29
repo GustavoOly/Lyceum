7029f90e137120afa1ae715a1bd8ba6c9337096f
Gustavo Oly <gustavooly.dev@gmail.com>
2024-04-29 10:18:24 -0300

    feat(built-in Functions): calloc e malloc

--- built-in-functions/calloc.c
+++ built-in-functions/calloc.c
@@ -2,18 +2,17 @@
 #include <stdlib.h>
 
 int main() {
-
     int *x;
-    x = malloc(sizeof(int));
-
+    x = calloc(2, sizeof(int));
     if (x != NULL) {
         printf("Alocação bem sucedida\n");
-        printf("%i\n", *x);
-        *x = 50;
-        printf("%i\n", *x);
+        printf("%i\n", x[0]);
+        x[0] = 50;
+        x[1] = 60;
+        printf("%i\n", x[0]);
+        printf("%i\n", x[1]);
     } else {
         printf("Alocação mal sucedida\n");
     }
-
     return 0;
 }
