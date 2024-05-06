0646442fe8f6566977c01124995dd2935ce7c183
Gustavo Oly <gustavooly.dev@gmail.com>
2024-05-06 14:14:11 -0300

    feat: ascii

--- size.c
+++ size.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/_types.h>
 
 int main() {
     char *tamanho_caractere;
-    int tamanho_inteiro;
+    unsigned int tamanho_inteiro;
 
     tamanho_caractere = "dsdsds dsad sdasdas";
 
     char monstrar_tamanho_caractere = strlen(tamanho_caractere);
 
     printf("Tamanho caractere: %d\n", monstrar_tamanho_caractere);
+
 
     tamanho_inteiro = sizeof(int);
 
--- variáveis/variaveis.c
+++ variáveis/variaveis.c
@@ -14,6 +14,7 @@ void somar1() {
     y++;
 }
 
+
 int main() {
 
     somar1();
