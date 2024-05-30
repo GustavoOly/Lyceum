f4f0e3d694b255bc0f79a19fb1d9a5cac8fc6885
Gustavo Oly <gustavooly.dev@gmail.com>
2024-05-30 14:01:55 -0300

    feat: lista linear

--- Estrutura de dados/listas/lista_linear.c
+++ Estrutura de dados/listas/lista_linear.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// lista linear é um estrutura de dados que armazena elementos em uma sequência linear, cada elemento tem um predecessor e um sucessor (exeto o primeiro e o ultimo elemento).
+/*
+ lista linear é um estrutura de dados que armazena elementos em uma sequência
+ linear cada elemento tem um predecessor e um sucessor (exeto o primeiro e o
+ ultimo elemento).*/
 
-int main(){
-    
-    return 0;
-}
+int main() { return 0; }
