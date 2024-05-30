40bccd700bcdf07b6da838d73c14a741ce3af1c0
Gustavo Oly <gustavooly.dev@gmail.com>
2024-05-30 14:16:43 -0300

    feat: vetor dinamico com malloc

--- Estrutura de dados/estruturas_de_dados_homogeneas/vetores/vetor_dinamico.c
+++ Estrutura de dados/estruturas_de_dados_homogeneas/vetores/vetor_dinamico.c
@@ -6,18 +6,24 @@ int main() {
     puts("digite o tamanho do array: ");
     scanf("%i", &n);
 
+    // Aloca memória dinamicamente para o array de inteiros
     int *array = (int *)malloc(n * (sizeof(int)));
 
+    // Atribui valores aos elementos do array
     for (int i = 0; i < n; i++) {
         array[i] = i * 2;
     }
 
-    puts("exibir valores");
+    // mostra todos os valores de cada elemento da variável de ponteiro array
+    puts("Exibir valores:");
     for (int i = 0; i < n; i++) {
         printf("%i\n", array[i]);
     }
 
+    // Libera a memória alocada pelo array
     free(array);
 
+    // Atribui NULL ao ponteiro para evitar acesso a memória não alocada
+    array = NULL;
     return 0;
 }
