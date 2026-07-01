f2c891488740497bf05d742711c9eb0b68f82142
Gustavo Oly <gustavooly.dev@gmail.com>
2026-07-01 19:42:48 -0300

    feat: aprimorar função de conversão de base para suportar base 3 e corrigir mensagens de erro

--- algoritmos/Math/math.h
+++ algoritmos/Math/math.h
@@ -1,6 +1,7 @@
 #ifndef MATH_H
 #define MATH_H
 #include <stdio.h>
+#include <string.h>
 
 static inline int soma(int a, int b) { return a + b; }
 static inline int subtracao(int a, int b) { return a - b; }
@@ -25,7 +26,15 @@ static inline int divisao(int a, int b) {
 static inline int conversaoDeBase(int a, int b) {
     switch (b) {
     case 2: {
-        int exp[2 * 9 - 1] = {};
+        int temp = a;
+        int size = 0;
+        while (temp >= 1) {
+            size++;
+            temp /= 2;
+        }
+        int exp[size];
+        memset(exp, 0, sizeof(exp));
+
         int aux = a;
         int i = 0;
 
@@ -52,8 +61,45 @@ static inline int conversaoDeBase(int a, int b) {
         }
         break;
     }
+    case 3: {
+        int temp = a;
+        int size = 0;
+        while (temp >= 1) {
+            size++;
+            temp /= 3;
+        }
+        int exp[size];
+        memset(exp, 0, sizeof(exp));
+
+        int aux = a;
+        int i = 0;
+
+        // Divisão
+        while (aux >= 1) {
+            int quo = aux / b;
+            int res = aux % b;
+            aux = quo;
+            exp[i] = res;
+            printf("%d", exp[i]);
+            i++;
+        }
+        puts("");
+
+        // Inversão
+        for (int j = 0; j < i / 2; ++j) {
+            int aux = exp[j];
+            exp[j] = exp[(i - 1) - j];
+            exp[(i - 1) - j] = aux;
+        }
+
+        // resultado
+        for (int j = 0; j < i; ++j) {
+            printf("%d", exp[j]);
+        }
+        break;
+    }
     default:
-        printf("Base invalida. Tente por 2");
+        printf("Base invalida. Tente por 2, 3");
         break;
     }
     return 0;
